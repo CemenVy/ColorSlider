d78cc79d44c0ad2d3d931784b1deae658d30ec22
CemenVy <semen.vydrin0108@gmail.com>
2023-07-28 14:55:29 +0400

    Work in logic and UI

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -14,11 +14,60 @@ class ViewController: UIViewController {
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLAbel: UILabel!
     
+    private var redValueColor = 0.0
+    private var greenValueColor = 0.0
+    private var blueValueColor = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        resultWindowView.backgroundColor = UIColor(
+            red: redValueColor,
+            green: greenValueColor,
+            blue: blueValueColor,
+            alpha: 1.0
+        )
+        
+        resultWindowView.layer.cornerRadius = 15
     }
 
 
-
+    @IBAction func redSlider(_ sender: UISlider) {
+        redValueColor = CGFloat(sender.value)
+        
+        redValueLabel.text = String(format: "%.2f", redValueColor)
+        resultWindowView.backgroundColor = UIColor(
+            red: redValueColor,
+            green: greenValueColor,
+            blue: blueValueColor,
+            alpha: 1.0
+        )
+    }
+    
+    @IBAction func greenSlider(_ sender: UISlider) {
+        greenValueColor = CGFloat(sender.value)
+        
+        greenValueLabel.text = String(format: "%.2f", greenValueColor)
+        resultWindowView.backgroundColor = UIColor(
+            red: redValueColor,
+            green: greenValueColor,
+            blue: blueValueColor,
+            alpha: 1.0
+        )
+    }
+    
+    @IBAction func blueSlider(_ sender: UISlider) {
+        blueValueColor = CGFloat(sender.value)
+        
+        blueValueLAbel.text = String(format: "%.2f", blueValueColor)
+        resultWindowView.backgroundColor = UIColor(
+            red: redValueColor,
+            green: greenValueColor,
+            blue: blueValueColor,
+            alpha: 1.0
+        )
+    }
+    
+    
 }
 
