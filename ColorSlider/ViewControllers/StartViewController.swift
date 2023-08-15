f85e3cd9b269a4552c1994e65e8d549e5439d673
CemenVy <semen.vydrin0108@gmail.com>
2023-08-15 20:04:13 +0400

    Added StartViewController

---
+++ ColorSlider/ViewControllers/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StartViewController.swift
+//  ColorSlider
+//
+//  Created by Семен Выдрин on 15.08.2023.
+//
+
+import UIKit
+
+final class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
