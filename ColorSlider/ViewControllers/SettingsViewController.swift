ad9c8169e0f0c9478640ff1e4bcd6a224a6f0d14
CemenVy <semen.vydrin0108@gmail.com>
2023-08-16 13:16:58 +0400

    Work with logic and UI

--- ColorSlider/ViewControllers/SettingsViewController.swift
+++ ColorSlider/ViewControllers/SettingsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class SettingsViewController: UIViewController, UITextFieldDelegate {
+final class SettingsViewController: UIViewController {
     
     // MARK: - IB Outlets
     @IBOutlet private var resultWindowView: UIView!
@@ -20,9 +20,7 @@ final class SettingsViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet private var greenSlider: UISlider!
     @IBOutlet private var blueSlider: UISlider!
     
-    @IBOutlet var redValueTextField: UITextField!
-    @IBOutlet var greenValueTextField: UITextField!
-    @IBOutlet var blueValueTextField: UITextField!
+    @IBOutlet private var textFields: [UITextField]!
     
     var colorStartScreen:UIColor!
     
@@ -32,15 +30,9 @@ final class SettingsViewController: UIViewController, UITextFieldDelegate {
         
         resultWindowView.layer.cornerRadius = 15
         updateSliderValue()
-        updateColorView()
         
-        redValueLabel.text = string(from: redSlider)
-        greenValueLabel.text = string(from: greenSlider)
-        blueValueLabel.text = string(from: greenSlider)
+        setupTextFields()
         
-        redValueTextField.delegate = self
-        greenValueTextField.delegate = self
-        blueValueTextField.delegate = self
     }
     
     // MARK: - IB Actions
@@ -49,11 +41,18 @@ final class SettingsViewController: UIViewController, UITextFieldDelegate {
         switch sender {
         case redSlider:
             redValueLabel.text = string(from: redSlider)
+            textFields[0].text = string(from: redSlider)
         case greenSlider:
             greenValueLabel.text = string(from: greenSlider)
+            textFields[1].text = string(from: greenSlider)
         default:
             blueValueLabel.text = string(from: blueSlider)
+            textFields[2].text = string(from: blueSlider)
         }
+    }
+    
+    @IBAction func saveValueButtonTapped() {
+        dismiss(animated: true)
     }
     
     // MARK: - Private Properties
@@ -64,6 +63,12 @@ final class SettingsViewController: UIViewController, UITextFieldDelegate {
             blue: blueSlider.value.cgFloat(),
             alpha: 1.0
         )
+    }
+    
+    private func updateValueLabel(){
+        redValueLabel.text = string(from: redSlider)
+        greenValueLabel.text = string(from: greenSlider)
+        blueValueLabel.text = string(from: blueSlider)
     }
     
     private func string(from slider: UISlider) -> String {
@@ -83,29 +88,59 @@ final class SettingsViewController: UIViewController, UITextFieldDelegate {
                 blueSlider.value = Float(blue)
             }
         }
+        updateValueLabel()
+        updateColorView()
     }
     
-    private func showAlert(withTitle title: String, andMessage message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            self.redValueTextField.text = ""
-            self.greenValueTextField.text = ""
-            self.blueValueTextField.text = ""
-        }
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
-    
-    }
-    
-  
 }
-
+// MARK: - UITextFieldDelegate
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let floatValue = Float(textField.text ?? ""), (0.0...1.0).contains(floatValue) else {
+            let alert = UIAlertController(
+                title: "Ошибка",
+                message: "Введите значение в диапазоне от 0.0 до 1.0",
+                preferredStyle: .alert
+            )
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        if let index = textFields.firstIndex(of: textField) {
+            switch index {
+            case 0:
+                redSlider.value = floatValue
+            case 1:
+                greenSlider.value = floatValue
+            default:
+                blueSlider.value = floatValue
+            }
+            updateColorView()
+            updateValueLabel()
+        }
+    }
+    
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            for textField in textFields {
+                textField.resignFirstResponder()
+            }
+            return true
+        }
+        
+        func setupTextFields() {
+            for textField in textFields {
+                textField.delegate = self
+            }
+        }
+}
 
 extension Float {
     func cgFloat() -> CGFloat {
         CGFloat(self)
     }
 }
+
